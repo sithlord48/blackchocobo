805d3d4289f245562727a0175ca3594e478e5012
sithlord48 <sithlord48@users.noreply.github.com>
2010-10-05 12:13:41 +0000

    dialog settings now open to set folder when browsing for a new folder

--- options.cpp
+++ options.cpp
@@ -30,24 +30,24 @@ void Options::changeEvent(QEvent *e)
 
 void Options::on_btn_set_save_pc_clicked()
 {
-    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Save FF7 PC Saves"),"");
+    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Save FF7 PC Saves"),bchoco_settings.value("save_pc_path").toString());
     bchoco_settings.setValue("save_pc_path",temp);
 }
 
 void Options::on_btn_set_save_emu_clicked()
 {
-    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Save mcd/mcr saves"),"");
+    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Save mcd/mcr saves"),bchoco_settings.value("save_emu_path").toString());
     bchoco_settings.setValue("save_emu_path",temp);
 }
 
 void Options::on_btn_set_load_pc_clicked()
 {
-    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Load FF7 PC Saves From"),"");
+    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Load FF7 PC Saves From"),bchoco_settings.value("load_path").toString());
     bchoco_settings.setValue("load_path",temp);
 }
 
 void Options::on_btn_set_export_pc_clicked()
 {
-    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Export FF7 PC Saves"),"");
+    QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Export FF7 PC Saves"),bchoco_settings.value("export_pc").toString());
     bchoco_settings.setValue("export_pc",temp);
 }
