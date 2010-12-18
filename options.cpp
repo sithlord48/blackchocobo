8ba529fe8eb8ec902e8b8bc83f3d4a892af0c451
sithlord48 <sithlord48@users.noreply.github.com>
2010-12-18 03:56:00 +0000

    editable text for paths

--- options.cpp
+++ options.cpp
@@ -31,51 +31,86 @@ void Options::changeEvent(QEvent *e)
 
 void Options::set_path_lbls()
 {
-    if (!bchoco_settings.value("default_save_file").isNull()){ui->lbl_defalut_save->setText(bchoco_settings.value("default_save_file").toString());}
-    if (!bchoco_settings.value("char_stat_folder").isNull()){ui->lbl_char_stat_folder->setText(bchoco_settings.value("char_stat_folder").toString());}
-    if (!bchoco_settings.value("save_pc_path").isNull()){ui->lbl_save_pc->setText(bchoco_settings.value("save_pc_path").toString());}
-    if (!bchoco_settings.value("save_emu_path").isNull()){ui->lbl_save_emu->setText(bchoco_settings.value("save_emu_path").toString());}
-    if (!bchoco_settings.value("load_path").isNull()){ui->lbl_load_path->setText(bchoco_settings.value("load_path").toString());}
-    if (!bchoco_settings.value("export_pc").isNull()){ui->lbl_export_pc->setText(bchoco_settings.value("export_pc").toString());}
+    if (!bchoco_settings.value("default_save_file").isNull()){ui->line_default_save->setText(bchoco_settings.value("default_save_file").toString());}
+    if (!bchoco_settings.value("char_stat_folder").isNull()){ui->line_char_stat_folder->setText(bchoco_settings.value("char_stat_folder").toString());}
+    if (!bchoco_settings.value("save_pc_path").isNull()){ui->line_save_pc->setText(bchoco_settings.value("save_pc_path").toString());}
+    if (!bchoco_settings.value("save_emu_path").isNull()){ui->line_save_emu->setText(bchoco_settings.value("save_emu_path").toString());}
+    if (!bchoco_settings.value("load_path").isNull()){ui->line_load_path->setText(bchoco_settings.value("load_path").toString());}
+    if (!bchoco_settings.value("export_pc").isNull()){ui->line_export_pc->setText(bchoco_settings.value("export_pc").toString());}
 }
 
+void Options::on_line_save_pc_editingFinished()
+{
+    if(!ui->line_save_pc->text().isNull()){bchoco_settings.setValue("save_pc",ui->line_save_pc->text());}
+    //set_path_lbls();
+}
 void Options::on_btn_set_save_pc_clicked()
 {
     QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Save FF7 PC Saves"),bchoco_settings.value("save_pc_path").toString());
     if(!temp.isNull()){bchoco_settings.setValue("save_pc_path",temp);}
-    set_path_lbls();
+    //set_path_lbls();
+}
+
+
+void Options::on_line_save_emu_editingFinished()
+{
+    if(!ui->line_save_emu->text().isNull()){bchoco_settings.setValue("save_emu",ui->line_save_emu->text());}
+    //set_path_lbls();
 }
 void Options::on_btn_set_save_emu_clicked()
 {
     QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Save mcd/mcr saves"),bchoco_settings.value("save_emu_path").toString());
     if(!temp.isNull()){bchoco_settings.setValue("save_emu_path",temp);}
-    set_path_lbls();
+    //set_path_lbls();
 }
 
-void Options::on_btn_set_load_pc_clicked()
+void Options::on_line_load_path_editingFinished()
+{
+    if(!ui->line_load_path->text().isNull()){bchoco_settings.setValue("load_path",ui->line_load_path->text());}
+    //set_path_lbls();
+}
+void Options::on_btn_set_load_path_clicked()
 {
     QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Load FF7 PC Saves From"),bchoco_settings.value("load_path").toString());
     if(!temp.isNull()){bchoco_settings.setValue("load_path",temp);}
-    set_path_lbls();
+    //set_path_lbls();
+}
+
+
+void Options::on_line_export_pc_editingFinished()
+{
+    if(!ui->line_export_pc->text().isNull()){bchoco_settings.setValue("export_pc",ui->line_export_pc->text());}
+    //set_path_lbls();
 }
 void Options::on_btn_set_export_pc_clicked()
 {
     QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Directory To Export FF7 PC Saves"),bchoco_settings.value("export_pc").toString());
     if(!temp.isNull()){bchoco_settings.setValue("export_pc",temp);}
-    set_path_lbls();
+    //set_path_lbls();
 }
 
+void Options::on_line_default_save_editingFinished()
+{
+    if(!ui->line_default_save->text().isNull()){bchoco_settings.setValue("default_save_file",ui->line_default_save->text());}
+    //set_path_lbls();
+}
 void Options::on_btn_set_default_save_clicked()
 {
     QString temp = QFileDialog::getOpenFileName(this,tr("Select A Default Save Game (Must Be Raw PSX)"),bchoco_settings.value("default_save_file").toString());
     if(!temp.isNull()){bchoco_settings.setValue("default_save_file",temp);}
-    set_path_lbls();
+    //set_path_lbls();
+}
+
+void Options::on_line_char_stat_folder_editingFinished()
+{
+    if(!ui->line_char_stat_folder->text().isNull()){bchoco_settings.setValue("char_stat_folder",ui->line_char_stat_folder->text());}
+    //set_path_lbls();
 }
 void Options::on_btn_set_char_stat_folder_clicked()
 {
     QString temp = QFileDialog::getExistingDirectory(this,tr("Select A Location To Save Character Stat Files"),bchoco_settings.value("char_stat_folder").toString());
     if(!temp.isNull()){bchoco_settings.setValue("char_stat_folder",temp);}
-    set_path_lbls();
+    //set_path_lbls();
 }
 /*~~~~~~~~~~~~~~~~CLOSE BUTTON~~~~~~~~~~~~~~~~~~*/
 void Options::on_pushButton_clicked()
--- options.h
+++ options.h
@@ -22,14 +22,20 @@ private:
     Ui::Options *ui;
 
 private slots:
-    void set_path_lbls();
+    void on_line_char_stat_folder_editingFinished();
+    void on_line_default_save_editingFinished();
+    void on_line_export_pc_editingFinished();
+    void on_line_load_path_editingFinished();
+    void on_line_save_emu_editingFinished();
+    void on_line_save_pc_editingFinished();
     void on_btn_set_char_stat_folder_clicked();
-    void on_pushButton_clicked();
     void on_btn_set_default_save_clicked();
     void on_btn_set_export_pc_clicked();
-    void on_btn_set_load_pc_clicked();
+    void on_btn_set_load_path_clicked();
     void on_btn_set_save_emu_clicked();
     void on_btn_set_save_pc_clicked();
+    void on_pushButton_clicked();
+    void set_path_lbls();
 };
 
 #endif // OPTIONS_H
