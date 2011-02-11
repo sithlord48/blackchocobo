9b88c7b7984d1d9c52b624b9767f620181e363ed
sithlord48 <sithlord48@users.noreply.github.com>
2011-02-11 02:36:56 +0000

    added the fix_pc_maskbyte function

--- globals.cpp
+++ globals.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************/
-//    copyright 2010 Chris Rizzitello <sithlord48@gmail.com>                //
+//    copyright 2010, 2011 Chris Rizzitello <sithlord48@gmail.com>                //
 //                                                                          //
 //    This file is part of Black Chocobo.                                   //
 //                                                                          //
@@ -16,7 +16,7 @@
 #include "globals.h"
 
 FF7 ff7;
-int* s;
+int s;
 
 int ff7__checksum( void* qw )
 {
@@ -37,4 +37,43 @@ int ff7__checksum( void* qw )
       r &= ( 1 << 16 ) - 1;
    }
    return (r^0xFFFF)&0xFFFF;
+}
+
+void fix_pc_bytemask(void)
+{
+quint8 mask=0;
+quint8 newheader[0x09] = {0x71,0x73,0x27,0x06,0x00,0x00,0x00,0x00,0x00};
+//calc 0x04 of the header (selected slot) no idea why they choose this way to do it but slot15 = 0xC2 , slot 14= 0xb2  and so on till slot2 = 0x01 and slot 01 0x00
+switch(s)
+{
+case 0: newheader[4]=0x00;
+case 1: newheader[4]=0x01;
+default:newheader[4]= (16 * (s-2))+2;
+};
+//calc 0x05 of the header (slots 1-8 empty?)
+for(int i=0;i<8;i++)
+{
+    if(ff7.SG_Region_String[i].contains("00867") || ff7.SG_Region_String[i].contains("00869") ||
+       ff7.SG_Region_String[i].contains("00900") || ff7.SG_Region_String[i].contains("94163") ||
+       ff7.SG_Region_String[i].contains("00700") || ff7.SG_Region_String[i].contains("01057"))
+    {
+    mask |= (1<<i);
+    }
+}
+
+newheader[5]=mask;
+mask=0;// reset for the next byte
+//calc 0x06 of the header (slot 9-15 empty?)
+for(int i=8;i<15;i++)
+{
+    if(ff7.SG_Region_String[i].contains("00867") || ff7.SG_Region_String[i].contains("00869") ||
+       ff7.SG_Region_String[i].contains("00900") || ff7.SG_Region_String[i].contains("94163") ||
+       ff7.SG_Region_String[i].contains("00700") || ff7.SG_Region_String[i].contains("01057"))
+    {
+    mask |= (1<<(i-8));
+    }
+}
+newheader[6]=mask;
+
+memcpy(ff7.file_headerp,newheader,9);
 }
