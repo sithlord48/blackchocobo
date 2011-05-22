2ebb4560a9d1a1584ca08eaef70e83df5aca4780
sithlord48 <sithlord48@users.noreply.github.com>
2011-05-22 20:11:20 +0000

    update version info and translation files. aboutbox version # no longer bold

--- lang/bchoco_es.ts
+++ lang/bchoco_es.ts
@@ -6206,6 +6206,10 @@ p, li { white-space: pre-wrap; }
         <source>New: .bin .mc .mci .ps .psm .dff  Format Support</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Fixed:Psx and Psv Region Detection</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>errbox</name>
