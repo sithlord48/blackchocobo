c502a722b80a944b704f2823b912c6543099aed2
sithlord48 <sithlord48@6078b701-62e0-4143-b5eb-4703695a1e3f>
2011-10-10 04:09:24 +0000

    new Text Class to convert ff7 strings. updated mainwinow and slotselect to use the new class will also display in japanese fonts (correctly?) and allow for saving w/ those chars (correctly?)

--- trunk/globals.h
+++ trunk/globals.h
@@ -22,12 +22,11 @@
 #include <QCoreApplication>
 
 int ff7__checksum(void * qw );
-
-void preptext (void);
 void fix_pc_bytemask(FF7&,int,bool);
 void fix_psx_header(FF7&,int);
 void fix_vmc_header(FF7&);
 QString avatar_style(int);
+
 class locations
 {
 public:
@@ -45,6 +44,19 @@ public:
     QString MateriaNames(int);
     QString MateriaStats(int);
     QString MateriaSkills(int,int);
+};
+
+class Text{
+private:
+    QString Char_list;
+    QString FF7_list;
+    QString eng;
+    QString jap;
+public:
+    Text();
+    void init(bool);
+    QString PC(int);
+    quint8 FF7(int);
 };
 
 struct LOCATION {//Location Table Stuff
@@ -258,264 +270,6 @@ static MATERIA Materias[]=
     {QT_TRANSLATE_NOOP("Materia_Names","Bahamut ZERO"),     ":/icon/summon",      0x58,/*{-10,+15,0,0,0,0,+4,+4}*/{35000,120000,150000,250000},2,5},
     {QT_TRANSLATE_NOOP("Materia_Names","Knights of Round"), ":/icon/summon",      0x59,/*{-10,+20,0,0,0,0,+8,+8}*/{50000,200000,300000,500000},2,5},
     {QT_TRANSLATE_NOOP("Materia_Names","Master Summon"),    ":/icon/summon",      0x5A,/*{0,0,0,0,0,0,0,0}*/{0,0,0,0},2,1}
-};
-const char chPC[256] = {
-        ' ',	// 00: Space
-        '!',
-        '"',
-        '#',
-        '$',
-        '%',
-        '&',
-        '\'',	// 07: "'" (apostrophe)
-        '(',
-        ')',
-        '*',
-        '+',
-        ',',
-        '-',
-        '.',
-        '/',
-        '0',
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        ':',
-        ';',
-        '<',
-        '=',
-        '>',
-        '?',
-        '@',
-        'A',
-        'B',
-        'C',
-        'D',
-        'E',
-        'F',
-        'G',
-        'H',
-        'I',
-        'J',
-        'K',
-        'L',
-        'M',
-        'N',
-        'O',
-        'P',
-        'Q',
-        'R',
-        'S',
-        'T',
-        'U',
-        'V',
-        'W',
-        'X',
-        'Y',
-        'Z',
-        '[',
-        '\\',
-(char)	']',
-        '^',
-        '_',
-        '`',
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z',
-        '{',
-        '|',
-        '}',
-        '~',
-        ' ',
-        'Ä',
-        'Á',
-        'Ç',
-        'É',
-        'Ñ',
-        'Ö',
-        'Ü',
-        'á',
-        'à',
-        'â',
-        'ä',
-        'ã',
-        'å',
-        'ç',
-        'é',
-        'è',
-        'ê',
-        'ë',
-        'í',
-        'ì',
-        'î',
-        'ï',
-        'ñ',
-        'ó',
-        'ò',
-        'ô',
-        'ö',
-        'õ',
-        'ú',
-        'ù',
-        'û',
-        'ü',
-        ' ',
-        '°',
-        '¢',
-        '£',
-        'Ú',
-        'Û',
-        '¶',
-        'ß',
-        '®',
-        '©',
-        '™',
-        '´',
-        '¨',
-        ' ',	// non-equality
-        'Æ',
-        'Ø',
-        ' ',	// infinity
-        '±',
-        ' ',	// <=
-        ' ',	// >=
-        '¥',
-        'µ',
-        ' ',	// a-like symbol
-        ' ',	// sum
-        ' ',	// capital pi
-        ' ',	// pi
-        ' ',	// J-like symbol
-        ' ',	// a-underline
-        ' ',	// o-underline
-        ' ',	// capital omega
-        'æ',
-        'ø',
-        '¿',
-        '¡',
-        '¬',
-        ' ',	// check mark
-        'ƒ',
-        ' ',	// approximate to
-        ' ',	// capital delta
-        '«',
-        '»',
-        '…',	// '...' character
-        ' ',
-        'À',
-        'Ã',
-        'Õ',
-        'Œ',
-        'œ',
-        '–',
-        '—',
-        '“',
-        '”',
-        '‘',
-        '’',
-        '÷',
-        ' ',	// romboid
-        'ÿ',
-        '/',
-        '¤',
-        '‹',
-        '›',
-        ' ',	// fi-like
-        ' ',	// fl-like
-        '•',	// filled square
-        '·',
-        '‚',
-        '„',
-        '‰',
-        'Å',
-        'Ê',
-        'Ë',
-        'Á',	// A-dash
-        'È',
-        'Í',
-        'Î',
-        'Ï',
-        'Ì',
-        'Ó',
-        'Ô',
-        ' ',	// apple
-        'Ò',
-        'Ú',
-        'Û',
-        ' ',	// wide U
-        ' ',	// low vertical dash
-        ' ',	// D5: Cyan (following FE)
-        ' ',
-        ' ',	// D7: Purple (following FE)
-        ' ',
-        ' ',	// D9: White (following FE)
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',	// E0
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',	// E7: Line feed (?)
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',	// F0
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',
-        ' ',	// FE: Change text color
-        '\0'	// FF: End of string (NULL)
 };
 #endif // GLOBALS_H
 
