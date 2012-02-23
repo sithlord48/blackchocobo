de500cd6fcafe71d12983dbb2e48ce77c6319440
vegetass4 <vegetass4@6078b701-62e0-4143-b5eb-4703695a1e3f>
2012-02-23 01:24:03 +0000

    svnrev minor change.

--- trunk/about.h
+++ trunk/about.h
@@ -10,7 +10,7 @@
 //                                                                          //
 //    Black Chocobo is distributed in the hope that it will be useful,      //
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
 //    GNU General Public License for more details.                          //
 /****************************************************************************/
 
@@ -19,6 +19,9 @@
 
 #ifndef SVNVERSION
 #include "svnversion.h"
+#ifndef SVNVERSION
+#define SVNVERSION
+#endif
 #endif
 
 #include <QDialog>
