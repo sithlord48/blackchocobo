388dddbf98c5ca15fd960019553f6680a84808e2
sithlord48 <sithlord48@users.noreply.github.com>
2012-07-30 17:14:02 +0000

    QListWidget now a SubClass of QTableWidget.

--- FF7tk/widgets/ItemList.cpp
+++ FF7tk/widgets/ItemList.cpp
@@ -1,28 +1,26 @@
 #include "ItemList.h"
-#include <QToolTip>
+//#include <QToolTip>
+//#include <QEvent>
 //#include <QMessageBox>
-ItemList::ItemList(QWidget *parent) :
-    QWidget(parent)
-{
-    QString style ="QTableWidget{selection-background-color:rgba(0, 0,0,0);}";
-    tbl_item = new QTableWidget(320,3);
-    tbl_item->setEditTriggers(QAbstractItemView::NoEditTriggers);// thats a long 0
-    tbl_item->setStyleSheet(style);
-    tbl_item->setContextMenuPolicy(Qt::CustomContextMenu);
-    tbl_item->setSelectionBehavior(QAbstractItemView::SelectRows);
-    //tbl_item->setSelectionMode(QAbstractItemView::NoSelection);
 
-    connect(tbl_item,SIGNAL(currentCellChanged(int,int,int,int)),this,SLOT(listSelectionChanged(int,int,int,int)));
-    connect(tbl_item,SIGNAL(customContextMenuRequested(QPoint)),this,SLOT(customContextMenuRequested(QPoint)));
-    tbl_item->horizontalHeader()->hide();
-    tbl_item->verticalHeader()->hide();
-    tbl_item->setColumnWidth(0,270);
-    tbl_item->adjustSize();
-    QHBoxLayout *layout = new QHBoxLayout;
-    layout->addWidget(tbl_item);
-    layout->setContentsMargins(0,0,0,0);
-    this->setLayout(layout);
-    this->adjustSize();
+ItemList::ItemList(QWidget *parent) :
+    QTableWidget(parent)
+{
+    QString style ="selection-background-color:rgba(0, 0,0,0);";
+    //tbl_item = new QTableWidget(320,3);
+    setRowCount(320);
+    setColumnCount(3);
+    setEditTriggers(QAbstractItemView::NoEditTriggers);// thats a long 0
+    setStyleSheet(style);
+    setContextMenuPolicy(Qt::CustomContextMenu);
+    setSelectionBehavior(QAbstractItemView::SelectRows);
+    //setSelectionMode(QAbstractItemView::NoSelection);
+    connect(this,SIGNAL(currentCellChanged(int,int,int,int)),this,SLOT(listSelectionChanged(int,int,int,int)));
+    connect(this,SIGNAL(customContextMenuRequested(QPoint)),this,SLOT(customContextMenuRequested(QPoint)));
+    horizontalHeader()->hide();
+    verticalHeader()->hide();
+    setColumnWidth(0,270);
+    adjustSize();
     createdSelector = false;
     for(int i=0;i<320;i++){itemlist.append(0xFFFF);}//initlize the data.
 }
@@ -41,64 +39,64 @@ void ItemList::itemselector_changed(quint16 item)
 void ItemList::customContextMenuRequested(const QPoint &pos)
 {
         item_preview = new ItemPreview(this);
-        item_preview->setItem(Items.itemId(itemlist.at(tbl_item->currentRow())));
-        item_preview->exec(tbl_item->mapToGlobal(pos));
+        item_preview->setItem(Items.itemId(itemlist.at(currentRow())));
+        item_preview->exec(mapToGlobal(pos));
 }
 
 void ItemList::itemupdate()
 {
     QTableWidgetItem *newItem;
-    int j= tbl_item->currentRow();
-    tbl_item->reset(); // just incase
-    tbl_item->clearContents();
-    tbl_item->setColumnWidth(0,40);
-    tbl_item->setColumnWidth(1,172);
-    tbl_item->setColumnWidth(2,55);
-    tbl_item->setRowCount(320);
+    int j= currentRow();
+    reset(); // just incase
+    clearContents();
+    setColumnWidth(0,40);
+    setColumnWidth(1,172);
+    setColumnWidth(2,55);
+    setRowCount(320);
 
     for (int i=0;i<320;i++) // set up items
     {
         if (Items.itemQty(itemlist.at(i)) == 0x7F && Items.itemId(itemlist.at(i)) == 0x1FF)
         {
             newItem = new QTableWidgetItem("",0);
-            tbl_item->setItem(i,0,newItem);
+            setItem(i,0,newItem);
             newItem = new QTableWidgetItem(tr("-------EMPTY--------"),0);
             newItem->setToolTip("");
-            tbl_item->setItem(i,1,newItem);
-            tbl_item->setRowHeight(i,22);
+            setItem(i,1,newItem);
+            setRowHeight(i,22);
             newItem = new QTableWidgetItem("",0);
-            tbl_item->setItem(i,2,newItem);
+            setItem(i,2,newItem);
         }
         else if(Items.itemId(itemlist.at(i)) > 319)
         {
             newItem = new QTableWidgetItem("",0);
-            tbl_item->setItem(i,0,newItem);
+            setItem(i,0,newItem);
             newItem = new QTableWidgetItem(tr("-------BAD ID-------"),0);
             newItem->setToolTip("");
-            tbl_item->setItem(i,1,newItem);
-            tbl_item->setRowHeight(i,22);
+            setItem(i,1,newItem);
+            setRowHeight(i,22);
             newItem = new QTableWidgetItem("",0);
-            tbl_item->setItem(i,2,newItem);
+            setItem(i,2,newItem);
         }
         else
         {
             QString qty;
             //Replaced by new item engine. (Vegeta_Ss4)
             newItem = new QTableWidgetItem(Items.Icon(Items.itemId(itemlist.at(i))),"",0);
-            tbl_item->setItem(i,0,newItem);
+            setItem(i,0,newItem);
             newItem = new QTableWidgetItem(Items.Name(Items.itemId(itemlist.at(i))),0);
             newItem->setToolTip(Items.Desc(Items.itemId(itemlist.at(i))));
-            tbl_item->setItem(i,1, newItem);
-            tbl_item->setRowHeight(i,22);
+            setItem(i,1, newItem);
+            setRowHeight(i,22);
             newItem = new QTableWidgetItem(qty.setNum(Items.itemQty(itemlist.at(i))),0);
-            tbl_item->setItem(i,2,newItem);
+            setItem(i,2,newItem);
 
         }
     }
-    tbl_item->blockSignals(true);
-    tbl_item->setCurrentCell(j,0);
-    if(createdSelector==false){tbl_item->clearSelection();}//if there is no Selector made its because of a new item list.
-    tbl_item->blockSignals(false);
+    blockSignals(true);
+    setCurrentCell(j,0);
+    if(createdSelector==false){clearSelection();}//if there is no Selector made its because of a new item list.
+    blockSignals(false);
 }
 void ItemList::listSelectionChanged(int row,int colum,int prevRow,int prevColum)
 {
@@ -111,12 +109,12 @@ void ItemList::listSelectionChanged(int row,int colum,int prevRow,int prevColum)
     itemselector->setMinimumWidth(270);
     itemselector->setObjectName(QString::number(row));
     QTableWidgetItem *newItem = new QTableWidgetItem("",0);
-    tbl_item->setItem(row,0,newItem);
+    setItem(row,0,newItem);
     newItem = new QTableWidgetItem("",0);//can't put the same item in two places.
-    tbl_item->setItem(row,1,newItem);
+    setItem(row,1,newItem);
     newItem = new QTableWidgetItem("",0);//can't put the same item in two places.
-    tbl_item->setItem(row,2,newItem);
-    tbl_item->setCellWidget(row,0,itemselector);
+    setItem(row,2,newItem);
+    setCellWidget(row,0,itemselector);
     itemselector->setCurrentItem(itemlist.at(row));
     connect(itemselector,SIGNAL(item_changed(quint16)),this,SLOT(itemselector_changed(quint16)));
 }
